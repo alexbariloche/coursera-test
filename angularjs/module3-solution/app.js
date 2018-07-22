a9ac802c4ae4383204f017d36b44943d1f304ec9
Alex Achterberg <alex.bariloche@gmail.com>
2018-07-22 19:05:46 -0300

    App code and AngularJS lib

---
+++ angularjs/module3-solution/app.js
@@ -0,0 +1,55 @@
+(function () {
+
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', narrowItDownController)
+.service('MenuSearchService', menuSearchService)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+
+// Controller
+narrowItDownController.$inject = ['MenuSearchService'];
+function narrowItDownController(menuSearchService) {
+  var narrower = this;
+  narrower.found = [];
+
+  narrower.getMatchedMenuItems = function () {
+    var promise = menuSearchService.getMatchedMenuItems(narrower.searchTerm);
+    promise.then(function (itemsMatched) {
+      narrower.found = itemsMatched;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  };
+};
+
+// Service
+menuSearchService.$inject = ['$http', 'ApiBasePath'];
+function menuSearchService($http, ApiBasePath) {
+  var searcher = this;
+
+  searcher.getMatchedMenuItems = function (searchTerm) {
+
+    var promise = $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json")
+    });
+
+    return promise.then( function (menuItems) {
+      var itemsFound = [];
+      for (var item in menuItems.data.menu_items) {
+        var itemDesc = menuItems.data.menu_items[item].description;
+        if ( itemDesc.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1) {
+          itemsFound.push(menuItems.data.menu_items[item]);
+        }
+      }
+      return itemsFound;
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  };
+};
+
+})();
