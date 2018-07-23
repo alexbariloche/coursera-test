c154fdc9e75b82535225336279a87c2438e2e0a5
Alex Achterberg <alex.bariloche@gmail.com>
2018-07-23 00:30:14 -0300

    Struggling with directive

--- angularjs/module3-solution/app.js
+++ angularjs/module3-solution/app.js
@@ -3,9 +3,10 @@
 'use strict';
 
 angular.module('NarrowItDownApp', [])
-.controller('NarrowItDownController', narrowItDownController)
-.service('MenuSearchService', menuSearchService)
-.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+  .controller('NarrowItDownController', narrowItDownController)
+  .service('MenuSearchService', menuSearchService)
+  .directive('foundItems', foundItemsDirective)
+  .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
 // Controller
 narrowItDownController.$inject = ['MenuSearchService'];
@@ -30,26 +31,35 @@ function menuSearchService($http, ApiBasePath) {
   var searcher = this;
 
   searcher.getMatchedMenuItems = function (searchTerm) {
-
     var promise = $http({
       method: "GET",
       url: (ApiBasePath + "/menu_items.json")
     });
-
-    return promise.then( function (menuItems) {
-      var itemsFound = [];
+    return promise.then(function (menuItems) {
+      var items = [];
       for (var item in menuItems.data.menu_items) {
         var itemDesc = menuItems.data.menu_items[item].description;
         if ( itemDesc.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1) {
-          itemsFound.push(menuItems.data.menu_items[item]);
+          items.push(menuItems.data.menu_items[item]);
         }
       }
-      return itemsFound;
+      return items;
     })
     .catch(function (error) {
       console.log(error);
     })
   };
+};
+
+function foundItemsDirective() {
+  var ddo = {
+    templateUrl: 'found-items.html',
+    scope: {
+      foundItems: '<',
+      onRemove: '&'
+    }
+  };
+  return ddo;
 };
 
 })();
