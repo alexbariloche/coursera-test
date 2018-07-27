be0f7a73463e5748fdf5b1221f7b272051868bee
Alex Achterberg <alex.bariloche@gmail.com>
2018-07-26 22:35:00 -0300

    Directives for main content. Json's for directive templates content via directive controller with  service. Bootstrap content handling.

--- nuevo/js/app.js
+++ nuevo/js/app.js
@@ -9,9 +9,6 @@ var showLoading = function (selector) {
 };
 
 // Main page content snippets
-var homeHtmlUrl = "snippets/home-snippet.html";
-var MisasHtmlUrl = "snippets/misas-snippet.html";
-var HomiliasHtmlUrl = "snippets/homilias-snippet.html";
 
 // Function that inserts content at html element
 var insertHtml = function (selector, html) {
@@ -23,7 +20,7 @@ var insertHtml = function (selector, html) {
 document.addEventListener("DOMContentLoaded", function (event) {
   showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
-    homeHtmlUrl,
+    "snippets/home-snippet.html",
     function (homeHtml) {
       insertHtml("#main-content", homeHtml);
     },
@@ -40,48 +37,70 @@ $(function () {
   });
 });
 
-var homilias = {
-  "actuales":
-  [
-    {
-      "tiempo": "Pascual",
-      "descripcion": "Domingo XI",
-      "fecha" : "08/07/2018",
-      "audio" : "media/homilias/Domingo XI Tiempo Ordinario 2018.mp3"
-    },
-    {
-      "tiempo": "Ordinario",
-      "descripcion": "Domingo X",
-      "fecha" : "01/07/2018",
-      "audio" : "media/homilias/Domingo X Tiempo Ordinario 2018.mp3"
-    }
-  ],
-  "historicas":
-  [
-  ]
-};
-
 // AngularJS code
 angular.module('SanEduardoApp', [])
   .controller('MenuController', menuController)
-  .service('MenuService', menuService);
+  .service('MenuService', menuService)
+  .directive('mainContent', mainContentDirective)
+  .directive('misasContent', misasContentDirective)
+  .directive('homiliasContent', homiliasContentDirective);
 
-menuController.$inject = ['MenuService'];
-function menuController( menuService) {
-  var menuCtrl = this;
-
-  menuCtrl.Inicio = function () {
-    menuService.GetMainContent(homeHtmlUrl);
+// Main directive
+function mainContentDirective() {
+  var ddo = {
+    templateUrl: "snippets/home-snippet.html"
   };
+  return ddo;
+}
 
-  menuCtrl.Misas = function () {
-    menuService.GetMainContent(MisasHtmlUrl);
-  };
+// Misas directive
+function misasContentDirective() {
+  var ddo = {
+    templateUrl: "snippets/misas-snippet.html",
+    controller: misasContentDirectiveController,
+    controllerAs: 'misas',
+    bindToController: true
+};
+  return ddo;
+}
 
-  menuCtrl.Homilias = function () {
-    menuService.GetMainContent(HomiliasHtmlUrl);
+misasContentDirectiveController.$inject = [ '$http'];
+function misasContentDirectiveController( $http) {
+  var misas = this;
+
+  // Get Json with Masses to offer
+  $http ({
+    method: 'GET',
+    url: 'media/misas.json'
+  }).then( function (response) {
+    misas.list = response.data;
+  }, function (error) {
+    console.log(error);
+  });
+}
+
+// Homilies directive
+function homiliasContentDirective() {
+  var ddo = {
+    templateUrl: "snippets/homilias-snippet.html",
+    controller: homiliasContentDirectiveController,
+    controllerAs: 'homilias',
+    bindToController: true
   };
-  
+  return ddo;
+}
+
+homiliasContentDirectiveController.$inject = [ '$http'];
+function homiliasContentDirectiveController( $http) {
+  var homilias = this;
+
+  // Get Json with Homilies to offer
+  $http ({
+    method: 'GET',
+    url: 'media/homilias.json'
+  }).then( function (response) {
+    homilias.list = response.data;
+  });
 }
 
 menuService.$inject = [ '$http'];
@@ -98,6 +117,34 @@ function menuService( $http) {
       $("#collapsable-nav").collapse('hide');
     });
   };
+}
+
+menuController.$inject = ['MenuService'];
+function menuController( menuService) {
+  var menuCtrl = this;
+
+  menuCtrl.inicio = true;
+  menuCtrl.homilias = false;
+  menuCtrl.misas = false;
+
+  menuCtrl.Inicio = function () {
+    menuCtrl.inicio = true;
+    menuCtrl.homilias = false;
+    menuCtrl.misas = false;
+  };
+
+  menuCtrl.Misas = function () {
+    menuCtrl.inicio = false;
+    menuCtrl.homilias = false;
+    menuCtrl.misas = true;
+  };
+
+  menuCtrl.Homilias = function () {
+    menuCtrl.inicio = false;
+    menuCtrl.misas = false;
+    menuCtrl.homilias = true;
+  };
+
 }
 
 })();
