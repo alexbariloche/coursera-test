5f36f722580e99d00adce489ba4a37680e1320b1
alexbariloche <alex.bariloche@gmail.com>
2021-07-26 00:51:59 -0300

    cwd

--- aa.bat
+++ aa.bat
@@ -1,5 +1,9 @@
 echo "running aa.bat"
 @call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64 %*
+set CWD="cd"
+echo "original folder "
+echo $CWD
 cd .\\mca\\windows
 echo "in dir of interest"
 type main.cpp
+cd $CWD
