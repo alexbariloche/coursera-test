4e020d8022d7bedc64d0515f01c8af7bd7bb0871
alexbariloche <alex.bariloche@gmail.com>
2021-07-26 00:13:21 -0300

    is VC installed

--- aa.bat
+++ aa.bat
@@ -1,4 +1,5 @@
 echo "running aa.bat"
+@call "c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 %*
 cd .\\mca\\windows
 echo "in dir of interest"
 type main.cpp
