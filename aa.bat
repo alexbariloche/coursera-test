c6f02e4df259e01c1484aafaaa52ce7936bff976
alexbariloche <alex.bariloche@gmail.com>
2021-07-26 00:24:20 -0300

    with vs

--- aa.bat
+++ aa.bat
@@ -1,6 +1,5 @@
 echo "running aa.bat"
-@call "c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 %*
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64 %*
 cd .\\mca\\windows
 echo "in dir of interest"
 type main.cpp
-dir C:\\Program Files (x86)
